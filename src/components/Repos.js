45bb3b03b5b2050e5c9d34f9d4a89c1dd1bd6323
Miguel Cazzaniga <115377890+MiguelCazzaniga@users.noreply.github.com>
2023-12-16 20:47:53 -0300

    16/12 subida final del dia

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,15 +5,21 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 import { toContainHTML } from '@testing-library/jest-dom/dist/matchers';
 const Repos = () => {
   const {repos}=useContext(GithubContext)
-   let languages=repos.reduce((total,item)=>{
-    const {language}=item
+   const languages=repos.reduce((total,item)=>{
+    const {language,stargazers_count}=item
     if (!language)return total
-    if (!total[language]){total[language]={label:language,value:1}}
-    else{total[language]={...total[language],value:total[language].value+1}
+    if (!total[language]){total[language]={label:language,value:1,stars:stargazers_count}}
+    else{total[language]={...total[language],value:total[language].value + 1,
+                                             stars:total[language].stars+stargazers_count}
    }
     return total
    },{})
-   languages=Object.values(languages).sort((a,b)=>{return a.value - b.value}).slice(0,5)
+   const mostUsed=Object.values(languages).sort((a,b)=>{return b.value - a.value}).slice(0,5)
+   const mostPopular=Object.values(languages)
+   .sort((a,b)=>{return b.stars - a.stars})
+   .map((item)=>{return {...item,value:item.stars}})
+   .slice(0,5)
+
   
 
   
@@ -21,8 +27,9 @@ const Repos = () => {
   return <section className='section'>
 
       <Wrapper className='section-center'>
-        {/* <ExampleChart data={chartData}/> */}
-        <Pie3D data={languages}/>
+        <Pie3D data={mostUsed}/>
+        <div></div>
+        <Doughnut2D data={mostPopular}/>
       </Wrapper>
   </section>
   
