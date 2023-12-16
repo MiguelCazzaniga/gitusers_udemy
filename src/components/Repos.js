868840dd4cbee8907c56f3b05616620c9177a1ea
Miguel Cazzaniga <115377890+MiguelCazzaniga@users.noreply.github.com>
2023-12-16 13:04:19 -0300

    Actualizado al 1612 al mediodia

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import { toContainHTML } from '@testing-library/jest-dom/dist/matchers';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const {repos}=useContext(GithubContext)
+   let languajes=repos.reduce((total,item)=>{
+    const {languaje}=item
+    if (!languaje)return total
+    if (!total[languaje]){total[languaje]={label:languaje,value:1}}
+    else{total[languaje]={...total[languaje],value:total[languaje].value+1}
+   }
+    return total
+   },{})
+  const chartData = [
+    {
+      label: "Venezuela",
+      value: "290",
+    },
+    {
+      label: "Saudi",
+      value: "260",
+    },
+    {
+      label: "Canada",
+      value: "180",
+    },
+    {
+      label: "Iran",
+      value: "140",
+    },
+    {
+      label: "Russia",
+      value: "115",
+    },
+    {
+      label: "UAE",
+      value: "100",
+    },
+    {
+      label: "US",
+      value: "30",
+    },
+    {
+      label: "China",
+      value: "30",
+    },
+  ]
+
+  return <section className='section'>
+
+      <Wrapper className='section-center'>
+        {/* <ExampleChart data={chartData}/> */}
+        <Pie3D data={chartData}/>
+      </Wrapper>
+  </section>
+  
 };
 
 const Wrapper = styled.div`
